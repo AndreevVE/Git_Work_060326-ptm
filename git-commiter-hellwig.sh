f9e3fa08b926abe160c4ebf01c8a58d21fcaa306
Dmitriy <dmitriy.hellwig@gmail.com>
2026-06-16 12:01:38 +0200

    fix: check git directory

--- git-commiter-hellwig.sh
+++ git-commiter-hellwig.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
-
-# git-commiter.sh
+# git-commiter-hellwig.sh
 # Автоматический коммит изменений в репозитории git
+
+if [ ! -d ".git" ]; then
+    echo "Текущая директория не является git-репозиторием"
+    exit 1
+fi
 
 if git diff --quiet; then
     echo "Нет изменений для коммита"
