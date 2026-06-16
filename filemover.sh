0e0a720056d388c68366506075dd996adf2d88ee
Elena_Ukrainets <elenaukrainec28@gmail.com>
2026-06-16 10:13:23 +0200

    issue5

--- filemover.sh
+++ filemover.sh
@@ -13,8 +13,6 @@
 
 
 #5. Проверка, есть ли файлы с указанным расширением в исходной директории
-source_directory=DIR1
-file_extension=ssh
 files=$(find "$source_directory" -type f -name "$*.file_extension")
 if [ -z "$files" ]; then
 	echo "Ошибка, в дериктории '$source_directory' с расширением  '.$file_extension' не найдено."
